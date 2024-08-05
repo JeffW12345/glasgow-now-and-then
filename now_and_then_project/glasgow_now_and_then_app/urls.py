586b472971bb6938d7b39a8dcd4d29989a204577
JeffW12345 <watersjg@gmail.com>
2024-08-05 15:38:24 +0100

    Got app to run in DEBUG=False without loss of static files ahead of deployment.
    Also added requirements.txt

--- now_and_then_project/glasgow_now_and_then_app/urls.py
+++ now_and_then_project/glasgow_now_and_then_app/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 from . import views
 
 app_name = 'nowandthen'
 
-urlpatterns = [
+urlpatterns = ([
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
@@ -15,4 +15,6 @@ urlpatterns = [
     path('add_picture/', views.add_picture, name='add_picture'),
     path('era/<str:era>/', views.era_search_results, name='era_search_results'),
     path('search/', views.search_results, name='search_results'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+])
