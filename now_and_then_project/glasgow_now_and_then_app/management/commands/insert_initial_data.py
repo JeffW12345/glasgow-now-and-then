7ebbdd4426de8d193946e6364d0eb5b40cc8c4ff
JeffW12345 <watersjg@gmail.com>
2024-08-05 14:48:33 +0100

    Refactoring of dummy testing data functionality.

--- now_and_then_project/glasgow_now_and_then_app/management/commands/insert_initial_data.py
+++ now_and_then_project/glasgow_now_and_then_app/management/commands/insert_initial_data.py
@@ -13,46 +13,57 @@ class Command(BaseCommand):
           # Clear existing data
           self.stdout.write('Clearing existing data...')
 
-          # Clear PictureLikes
-          PictureLike.objects.all().delete()
-
-          # Clear ImageTags
-          ImageTag.objects.all().delete()
-
-          # Clear Comments
-          Comment.objects.all().delete()
-
-          # Clear Pictures
-          Picture.objects.all().delete()
-
-          # Clear Users
-          User.objects.all().delete()
+          self.delete_old_objects()
 
           self.stdout.write(self.style.SUCCESS('Successfully cleared existing data'))
 
-          # Create Users
-          user1 = User.objects.create_user(username='john_doe', email='john@example.com', password='password123')
-          user2 = User.objects.create_user(username='jane_smith', email='jane@example.com', password='password123')
+          user1, user2 = self.create_users()
 
-          # Create Pictures
-          picture1 = Picture.objects.create(user=user1, image='shared_pics/fireworks.jpg', title='Fireworks',
-                                            description='Stunning firework display', era='2010-2020')
-          picture2 = Picture.objects.create(user=user2, image='shared_pics/glasgow_cityscape_copy.jpg',
-                                            title='Quintessential Glasgow', description='As title', era='2000-2010')
+          picture1, picture2 = self.create_pictures(user1, user2)
 
-          # Create Comments
+          self.create_comments(picture1, picture2, user1, user2)
+
+          self.create_image_tags(picture1, picture2)
+
+          self.create_likes(picture1, picture2, user1, user2)
+
+          self.stdout.write(self.style.SUCCESS('Successfully inserted initial data'))
+
+     def create_likes(self, picture1, picture2, user1, user2):
+          PictureLike.objects.create(user=user1, image=picture2)
+          PictureLike.objects.create(user=user2, image=picture1)
+
+     def create_image_tags(self, picture1, picture2):
+          ImageTag.objects.create(picture=picture1, tag_label='fireworks')
+          ImageTag.objects.create(picture=picture1, tag_label='night')
+          ImageTag.objects.create(picture=picture2, tag_label='Glasgow')
+          ImageTag.objects.create(picture=picture2, tag_label='Maryhill')
+
+     def create_comments(self, picture1, picture2, user1, user2):
           Comment.objects.create(user=user1, image=picture1, comment_body='Amazing!')
           Comment.objects.create(user=user2, image=picture1, comment_body='Superb!')
           Comment.objects.create(user=user2, image=picture2, comment_body='I love this place!')
 
-          # Create ImageTags
-          ImageTag.objects.create(picture=picture1, tag_label='fireworks')
-          ImageTag.objects.create(picture=picture1, tag_label='night')
-          ImageTag.objects.create(picture=picture2, tag_label='Glasgow')
-          ImageTag.objects.create(picture=picture2, tag_label='cityscape')
+     def create_pictures(self, user1, user2):
+          picture1 = Picture.objects.create(user=user1, image='shared_pics/fireworks.jpg',
+                                            title='Fireworks',
+                                            description='Stunning firework display',
+                                            era='2010-2020')
+          picture2 = Picture.objects.create(user=user2,
+                                            image='shared_pics/View-from-kitchen-window-of-Maryhill-tenements.1970.jpg',
+                                            title='A view from a kitchen window, Maryhill 1970 something',
+                                            description='This takes me back',
+                                            era='1970s')
+          return picture1, picture2
 
-          # Create Likes
-          PictureLike.objects.create(user=user1, image=picture2)
-          PictureLike.objects.create(user=user2, image=picture1)
+     def create_users(self):
+          user1 = User.objects.create_user(username='john_doe', email='john@example.com', password='password123')
+          user2 = User.objects.create_user(username='jane_smith', email='jane@example.com', password='password123')
+          return user1, user2
 
-          self.stdout.write(self.style.SUCCESS('Successfully inserted initial data'))
+     def delete_old_objects(self):
+          PictureLike.objects.all().delete()
+          ImageTag.objects.all().delete()
+          Comment.objects.all().delete()
+          Picture.objects.all().delete()
+          User.objects.all().delete()
