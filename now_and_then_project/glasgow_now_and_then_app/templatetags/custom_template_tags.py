5f56fd68db43b181dddc4ebfdf8bc9f94a00ef74
JeffW12345 <watersjg@gmail.com>
2024-08-06 12:57:26 +0100

    Added type hints

--- now_and_then_project/glasgow_now_and_then_app/templatetags/custom_template_tags.py
+++ now_and_then_project/glasgow_now_and_then_app/templatetags/custom_template_tags.py
@@ -1,16 +1,21 @@
+from typing import Iterable
+
 from django import template
+from django.contrib.auth.models import User
+
+from ..models import PictureLike, ImageTag
 
 register = template.Library()
 
 @register.simple_tag
-def user_liked_picture(picture_likes, user, picture_id):
+def user_liked_picture(picture_likes: Iterable[PictureLike], user: User, picture_id: int) -> bool:
     for picture_like in picture_likes:
         if picture_like.user.username == user.username and picture_like.image.id == picture_id:
             return True
     return False
 
 @register.simple_tag
-def any_tags_for_picture(tags, picture_id):
+def any_tags_for_picture(tags: Iterable[ImageTag], picture_id: int) -> bool:
     for tag in tags:
         if tag.picture.id == picture_id:
             return True
--- now_and_then_project/glasgow_now_and_then_app/views.py
+++ now_and_then_project/glasgow_now_and_then_app/views.py
@@ -8,168 +8,176 @@ from .models import Picture, PictureLike, ImageTag
 from .forms import PictureForm, CommentForm, UserForm, PictureLikeForm, ImageTagForm
 
 
-def index(request):
-    context_dict = {}
-    return render(request, 'index.html', context=context_dict)
+def index(request: HttpRequest):
+     context_dict = {}
+     return render(request, 'index.html', context=context_dict)
 
-def register(request):
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            profile = user_form.save(commit=False)
-            profile.user = user
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors)
-    else:
-        user_form = UserForm()
-    return render(request,
-                  'register.html',
-                  context={'user_form': user_form,
-                           'registered': registered})
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+def register(request: HttpRequest) -> HttpResponse:
+     registered = False
+     if request.method == 'POST':
+          user_form = UserForm(request.POST)
+          if user_form.is_valid():
+               user = user_form.save()
+               user.set_password(user.password)
+               user.save()
+               profile = user_form.save(commit=False)
+               profile.user = user
+               if 'picture' in request.FILES:
+                    profile.picture = request.FILES['picture']
+               profile.save()
+               registered = True
+          else:
+               print(user_form.errors)
+     else:
+          user_form = UserForm()
+     return render(request,
+                   'register.html',
+                   context={'user_form': user_form,
+                            'registered': registered})
 
-        user = authenticate(username=username, password=password)
 
-        if user:
-            if user.is_active:
-                login(request, user)
-                return redirect(reverse('nowandthen:index'))
-            else:
-                return HttpResponse("Your nowandthen account is disabled.")
-        else:
-            print(f"Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'login.html')
+def user_login(request: HttpRequest) -> HttpResponse:
+     if request.method == 'POST':
+          username = request.POST.get('username')
+          password = request.POST.get('password')
+
+          user = authenticate(username=username, password=password)
+
+          if user:
+               if user.is_active:
+                    login(request, user)
+                    return redirect(reverse('nowandthen:index'))
+               else:
+                    return HttpResponse("Your nowandthen account is disabled.")
+          else:
+               print(f"Invalid login details: {username}, {password}")
+               return HttpResponse("Invalid login details supplied.")
+     else:
+          return render(request, 'login.html')
+
 
 @login_required
-def add_picture(request):
-    PictureTagFormSet = modelformset_factory(ImageTag, form=ImageTagForm, extra=3)  # Adjust the extra parameter as needed
-    if request.method == 'POST':
-        form = PictureForm(request.POST, request.FILES)
-        formset = PictureTagFormSet(request.POST)
-        if form.is_valid() and formset.is_valid():
-            picture = form.save(commit=False)
-            picture.user = request.user
-            picture.save()
-            for form in formset:
-                if form.cleaned_data:
-                    tag = form.save(commit=False)
-                    tag.picture = picture
-                    tag.save()
-            return redirect(reverse('nowandthen:index'))
-        else:
-            print(form.errors, formset.errors)
-    else:
-        form = PictureForm()
-        formset = PictureTagFormSet(queryset=ImageTag.objects.none())
-    return render(request, 'add_picture.html', {'form': form, 'formset': formset})
+def add_picture(request: HttpRequest) -> HttpResponse:
+     PictureTagFormSet = modelformset_factory(ImageTag, form=ImageTagForm,
+                                              extra=3)  # Adjust the extra parameter as needed
+     if request.method == 'POST':
+          form = PictureForm(request.POST, request.FILES)
+          formset = PictureTagFormSet(request.POST)
+          if form.is_valid() and formset.is_valid():
+               picture = form.save(commit=False)
+               picture.user = request.user
+               picture.save()
+               for form in formset:
+                    if form.cleaned_data:
+                         tag = form.save(commit=False)
+                         tag.picture = picture
+                         tag.save()
+               return redirect(reverse('nowandthen:index'))
+          else:
+               print(form.errors, formset.errors)
+     else:
+          form = PictureForm()
+          formset = PictureTagFormSet(queryset=ImageTag.objects.none())
+     return render(request, 'add_picture.html', {'form': form, 'formset': formset})
+
 
 def photo_feed(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
-        if 'add_comment' in request.POST:
-            form = CommentForm(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
-                image_id = form.cleaned_data['image_id']
-                image = get_object_or_404(Picture, id=image_id)
-                comment.image = image
-                comment.user = request.user
-                comment.save()
-
-        elif 'add_like' in request.POST:
-            form = PictureLikeForm(request.POST)
-            if form.is_valid():
-                image_id = form.cleaned_data['image_id']
-                user_id = form.cleaned_data['user_id']
-
-                if not PictureLike.objects.filter(image_id=image_id, user_id=user_id).exists():
-                    like = form.save(commit=False)
+     if request.method == 'POST':
+          if 'add_comment' in request.POST:
+               form = CommentForm(request.POST)
+               if form.is_valid():
+                    comment = form.save(commit=False)
+                    image_id = form.cleaned_data['image_id']
                     image = get_object_or_404(Picture, id=image_id)
-                    like.image = image
-                    like.user = request.user
-                    like.save()
+                    comment.image = image
+                    comment.user = request.user
+                    comment.save()
 
-    pictures = Picture.objects.all()
+          elif 'add_like' in request.POST:
+               form = PictureLikeForm(request.POST)
+               if form.is_valid():
+                    image_id = form.cleaned_data['image_id']
+                    user_id = form.cleaned_data['user_id']
 
-    comment_form = CommentForm()
-    picture_like_form = PictureLikeForm()
-    picture_likes = PictureLike.objects.all()
+                    if not PictureLike.objects.filter(image_id=image_id, user_id=user_id).exists():
+                         like = form.save(commit=False)
+                         image = get_object_or_404(Picture, id=image_id)
+                         like.image = image
+                         like.user = request.user
+                         like.save()
 
-    tags = ImageTag.objects.all()
+     pictures = Picture.objects.all()
 
-    return render(request, 'photo_feed.html', {
-        'pictures': pictures,
-        'comment_form': comment_form,
-        'picture_like_form': picture_like_form,
-        'picture_likes': picture_likes,
-        'tags': tags,
-    })
-def search_results(request):
-    query = request.GET.get('q', '')
-    if query:
-        tags = ImageTag.objects.filter(tag_label__icontains=query)
-        pictures = [tag.picture for tag in tags]
+     comment_form = CommentForm()
+     picture_like_form = PictureLikeForm()
+     picture_likes = PictureLike.objects.all()
 
-        comment_form = CommentForm()
-        picture_like_form = PictureLikeForm()
-        picture_likes = PictureLike.objects.all()
+     tags = ImageTag.objects.all()
 
-        tags = ImageTag.objects.all()
+     return render(request, 'photo_feed.html', {
+          'pictures': pictures,
+          'comment_form': comment_form,
+          'picture_like_form': picture_like_form,
+          'picture_likes': picture_likes,
+          'tags': tags,
+     })
 
-        return render(request, 'search_results.html', {
-            'pictures': pictures,
-            'comment_form': comment_form,
-            'picture_like_form': picture_like_form,
-            'picture_likes': picture_likes,
-            'tags': tags,
-            'search_query': query
-        })
 
-def era_search_results(request, era):
-    if era:
-        era_display_choices = {
-            'present_day': 'Present Day',
-            '2010-2020': '2010-2020',
-            '2000-2010': '2000-2010',
-            '1990s': '1990s',
-            '1980s': '1980s',
-            '1970s': '1970s',
-            '1960s_and_earlier': '1960s and earlier'
-        }
-        pictures = Picture.objects.filter(era__icontains=era)
-        # Translate the era field to human-readable format
-        human_readable_era = era_display_choices.get(era, era)
+def search_results(request: HttpRequest) -> HttpResponse:
+     query = request.GET.get('q', '')
+     if query:
+          tags = ImageTag.objects.filter(tag_label__icontains=query)
+          pictures = [tag.picture for tag in tags]
 
-        comment_form = CommentForm()
-        picture_like_form = PictureLikeForm()
-        picture_likes = PictureLike.objects.all()
+          comment_form = CommentForm()
+          picture_like_form = PictureLikeForm()
+          picture_likes = PictureLike.objects.all()
 
-        tags = ImageTag.objects.all()
+          tags = ImageTag.objects.all()
 
-        return render(request, 'era_search_results.html', {
-            'pictures': pictures,
-            'comment_form': comment_form,
-            'picture_like_form': picture_like_form,
-            'picture_likes': picture_likes,
-            'tags': tags,
-            'search_query': human_readable_era
-        })
+          return render(request, 'search_results.html', {
+               'pictures': pictures,
+               'comment_form': comment_form,
+               'picture_like_form': picture_like_form,
+               'picture_likes': picture_likes,
+               'tags': tags,
+               'search_query': query
+          })
+
+
+def era_search_results(request: HttpRequest, era: str) -> HttpResponse:
+     if era:
+          era_display_choices = {
+               'present_day': 'Present Day',
+               '2010-2020': '2010-2020',
+               '2000-2010': '2000-2010',
+               '1990s': '1990s',
+               '1980s': '1980s',
+               '1970s': '1970s',
+               '1960s_and_earlier': '1960s and earlier'
+          }
+          pictures = Picture.objects.filter(era__icontains=era)
+          # Translate the era field to human-readable format
+          human_readable_era = era_display_choices.get(era, era)
+
+          comment_form = CommentForm()
+          picture_like_form = PictureLikeForm()
+          picture_likes = PictureLike.objects.all()
+
+          tags = ImageTag.objects.all()
+
+          return render(request, 'era_search_results.html', {
+               'pictures': pictures,
+               'comment_form': comment_form,
+               'picture_like_form': picture_like_form,
+               'picture_likes': picture_likes,
+               'tags': tags,
+               'search_query': human_readable_era
+          })
+
 
 @login_required
-def user_logout(request):
-    logout(request)
-    return render(request, 'logout.html')
-    return render(request, 'logout.html')
+def user_logout(request: HttpRequest) -> HttpResponse:
+     logout(request)
+     return render(request, 'logout.html')
